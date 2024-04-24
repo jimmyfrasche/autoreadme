0ed6b42cc33a10573573e18ede9ca6b17358e5a5
jimmyfrasche <soapboxcicero@gmail.com>
2024-04-24 15:58:01 -0700

    add helper to simplify template selection

--- template.go
+++ template.go
@@ -12,5 +12,12 @@ func parseTemplate(src string) (*template.Template, error) {
 	return template.New("").Parse(src)
 }
 
+func parseTemplateOr(src []byte, defaultTemplate *template.Template) (*template.Template, error) {
+	if len(src) == 0 {
+		return defaultTemplate, nil
+	}
+	return parseTemplate(string(src))
+}
+
 //go:embed default.template
 var defaultTemplateSrc string
